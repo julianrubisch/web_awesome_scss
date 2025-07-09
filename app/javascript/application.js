a66fd9bd6c79c740cfc9023e12c0a4e7b655023c
Julian Rubisch <julian@julianrubisch.at>
2025-07-09 12:13:23 +0200

    Start  🚀

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+
+import "@awesome.me/webawesome/dist/components/button/button.js";
+
+// web awesome imports
+import { setBasePath } from "@awesome.me/webawesome/dist/webawesome.js";
+setBasePath("node_modules/@awesome.me/webawesome/dist");
